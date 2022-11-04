f2f92e38f5d4c919842d7a5d4bb14434528e5224
S Dinesh <d4dinesh52@gmail.com>
2022-11-04 21:25:17 +0530

    initial commit

---
+++ src/main/java/com/example/schoolManagement/SchoolManagementApplication.java
@@ -0,0 +1,10 @@
+package com.example.schoolManagement;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementApplication.class, args);
+	}
+}
